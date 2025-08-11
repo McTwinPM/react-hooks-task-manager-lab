3542d4c182e6b59f1c1f0aef0f9607a4a5ae1636
Michael McCormack <mcctwin@gmail.com>
2025-08-11 11:54:26 -0500

    Implement global state with useContext

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -2,5 +2,12 @@ import React, { createContext, useState } from "react";
 
 export const TaskContext = createContext();
 
+const [tasks, setTasks] = useState([]);
+
 export function TaskProvider({ children }) {
+  return (
+    <TaskContext.Provider value={{ tasks, setTasks }}>
+      {children}
+    </TaskContext.Provider>
+  );
 }
